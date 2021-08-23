77b20f9be70a73686a197b67810459fff96c02e1
selam <youremail@address.com>
2021-08-23 15:58:34 -0400

    Second version of this project

--- GITHUB_AND_VersionControl_Project/src/com/BackUpUsing_GIT/Gitclass.java
+++ GITHUB_AND_VersionControl_Project/src/com/BackUpUsing_GIT/Gitclass.java
@@ -5,6 +5,9 @@ public class Gitclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("GIT Practice for taking backup @@@");
+
+System.out.println("GIT Practice for taking backup @@@");
+
 	}
 
 }
